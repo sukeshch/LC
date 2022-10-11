7eaa5a551d5a40f88a670fc425b1399d54b311e8
Sukesh Cheripalli <sukesh4018@gmail.com>
2022-10-10 21:59:10 -0400

    Time: 6 ms (42.49%), Space: 7.2 MB (57.87%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -3,35 +3,35 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> result;
         // // a S1, a 
-        // int sz = 0;
-        // for(int i = 0; i < nums.size(); i++)
-        // {
-        //     sz = result.size();
-        //     for(int k = 0; k < sz; k++)
-        //     {
-        //         auto temp = result[k];
-        //         temp.push_back(nums[i]);
-        //         result.push_back(temp);
-        //     }
-        //     vector<int> c(1,nums[i]);
-        //     result.push_back(c);
-        // }
-        // vector<int> c;
-        // result.push_back(c);
-        int pw = pow(2, nums.size());
-        for(int i=0; i<pw; i++)
+        int sz = 0;
+        for(int i = 0; i < nums.size(); i++)
         {
-            bitset<10> b(i);
-            vector<int> current;
-            current.resize(b.count());
-            int ct = 0;
-            for(int i=0; i<nums.size(); i++)
+            sz = result.size();
+            for(int k = 0; k < sz; k++)
             {
-                if(b[i])
-                    current[ct++] = nums[i];
+                auto temp = result[k];
+                temp.push_back(nums[i]);
+                result.push_back(temp);
             }
-            result.push_back(current);
+            vector<int> c(1,nums[i]);
+            result.push_back(c);
         }
+        vector<int> c;
+        result.push_back(c);
+        // int pw = pow(2, nums.size());
+        // for(int i=0; i<pw; i++)
+        // {
+        //     bitset<10> b(i);
+        //     vector<int> current;
+        //     current.resize(b.count());
+        //     int ct = 0;
+        //     for(int i=0; i<nums.size(); i++)
+        //     {
+        //         if(b[i])
+        //             current[ct++] = nums[i];
+        //     }
+        //     result.push_back(current);
+        // }
         return result;
     }
 };
