c520adf62f43a8ce0474831d84313dd03fce0d59
Sukesh Cheripalli <sukesh4018@gmail.com>
2022-10-10 21:58:11 -0400

    Time: 0 ms (100.00%), Space: 7 MB (87.18%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -2,22 +2,38 @@ class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> result;
-        // a S1, a 
-        int sz = 0;
-        for(int i = 0; i < nums.size(); i++)
+        // // a S1, a 
+        // int sz = 0;
+        // for(int i = 0; i < nums.size(); i++)
+        // {
+        //     sz = result.size();
+        //     for(int k = 0; k < sz; k++)
+        //     {
+        //         auto temp = result[k];
+        //         temp.push_back(nums[i]);
+        //         result.push_back(temp);
+        //     }
+        //     vector<int> c(1,nums[i]);
+        //     result.push_back(c);
+        // }
+        // vector<int> c;
+        // result.push_back(c);
+        int pw = pow(2, nums.size());
+        for(int i=0; i<pw; i++)
         {
-            sz = result.size();
-            for(int k = 0; k < sz; k++)
+            bitset<10> b(i);
+            vector<int> current;
+            current.resize(b.count());
+            int ct = 0;
+            for(int i=0; i<nums.size(); i++)
             {
-                auto temp = result[k];
-                temp.push_back(nums[i]);
-                result.push_back(temp);
+                if(b[i])
+                    current[ct++] = nums[i];
             }
-            vector<int> c(1,nums[i]);
-            result.push_back(c);
+            result.push_back(current);
         }
-        vector<int> c;
-        result.push_back(c);
         return result;
     }
 };
+
+// 0 1 2 
