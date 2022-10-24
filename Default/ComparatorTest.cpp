f16375656710ef109d16f025b1b03d0fb8734959
Sukesh Cheripalli <sukesh4018@gmail.com>
2022-10-24 01:21:52 -0400

    Create ComparatorTest.cpp
    How to use Comparator in CPP
    // TODO: Lambda comparator for priority_queue

---
+++ Default/ComparatorTest.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class ComparatorTest
+{
+public:
+  int val;
+  string str;
+  ComparatorTest (int v, string s):val (v), str (s) {}
+
+  bool operator< (const ComparatorTest & p) const
+  {
+    if (val != p.val) return val < p.val;
+    return str < p.str;
+  }
+  bool operator> (const ComparatorTest & p) const
+  {
+    if (val != p.val) return val > p.val;
+    return str > p.str;
+  }
+  bool operator= (const ComparatorTest & p) const
+  {
+    return p.val == val && p.str == str;
+  }
+};
+
+void print (priority_queue < ComparatorTest, vector < ComparatorTest >,
+       std::greater < ComparatorTest >> q)
+{
+  auto copyq = q;
+  cout << "printing Q: \n";
+  while (!copyq.empty ())
+    {
+      cout << copyq.top ().val << " " << copyq.top ().str << "\n";
+      copyq.pop ();
+    }
+  cout << "end printing\n";
+}
+
+void print (priority_queue < ComparatorTest > q)
+{
+  auto copyq = q;
+  cout << "printing Q: \n";
+  while (!copyq.empty ())
+    {
+      cout << copyq.top ().val << " " << copyq.top ().str << "\n";
+      copyq.pop ();
+    }
+  cout << "end printing\n";
+}
+
+int main ()
+{
+  {
+    // priority_queue Comparator using class operator
+    // std::less<ComparatorTest> is default; 
+    priority_queue < ComparatorTest, vector < ComparatorTest >,
+      std::greater < ComparatorTest > >pq;
+    pq.push (ComparatorTest (1, "a"));
+    print (pq);
+    pq.push (ComparatorTest (1, "b"));
+    print (pq);
+    pq.push (ComparatorTest (2, "b"));
+    print (pq);
+    pq.push (ComparatorTest (2, "a"));
+    print (pq);
+  }
+//   {
+//     // priority_queue Comparator using inline comparator 
+//     auto sortRuleLambda = [] (ComparatorTest const& s1, ComparatorTest const& s2) { // calls lhs.operator<(rhs); 
+//                                 return s1 < s2; };
+//     priority_queue < ComparatorTest, vector<ComparatorTest>, decltype( sortRuleLambda )> pq;
+//     pq.push (ComparatorTest (1, "a"));
+//     print (pq);
+//     pq.push (ComparatorTest (1, "b"));
+//     print (pq);
+//     pq.push (ComparatorTest (2, "b"));
+//     print (pq);
+//     pq.push (ComparatorTest (2, "a"));
+//     print (pq);
+//   }
+
+
+  return 0;
+}
