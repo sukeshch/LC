33bcc04cca598ccffe9a4a97fc93fb7a300d9276
Sukesh Cheripalli <sukesh4018@gmail.com>
2022-10-26 00:00:35 -0400

    Time: 305 ms (65.44%), Space: 56.2 MB (49.00%) - LeetHub

---
+++ 0310-minimum-height-trees/0310-minimum-height-trees.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        if(n==1) return {0};
+        vector<int> innode(n);
+        vector<vector<int>> children(n+1,vector<int>());
+        // find leaf nodes
+        for(auto e : edges)
+        {
+            innode[e[0]]++;
+            innode[e[1]]++;
+            children[e[0]].push_back(e[1]);
+            children[e[1]].push_back(e[0]);
+        }
+        queue<int> q;
+        for(int i=0; i<n; i++)
+        {
+            if(innode[i] == 1)
+            {
+                q.push(i);
+            }
+        }
+        int sz = 0;
+        int iters = 0;
+        int nodesLeft = n;
+        while(nodesLeft > 2)
+        {
+            sz = q.size();
+            nodesLeft -= sz;
+            for(int i=0; i<sz; i++)
+            {
+                int node = q.front();
+                q.pop();
+                for(auto v: children[node])
+                {
+                    innode[v]--;
+                    if(innode[v] == 1)
+                    {
+                        q.push(v);
+                    }
+                }
+            }
+            iters++;
+        }
+        vector<int> result;
+        while(!q.empty())
+        {
+            // cout << "poping\n";
+            result.push_back(q.front()); q.pop();
+        }
+        return result;
+    }
+};
