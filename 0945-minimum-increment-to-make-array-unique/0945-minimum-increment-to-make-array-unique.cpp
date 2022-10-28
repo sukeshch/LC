3174b9a7ed275601a69ee775c7355d75e34fcb1a
Sukesh Cheripalli <sukesh4018@gmail.com>
2022-10-28 03:06:30 -0400

    Time: 483 ms (19.90%), Space: 65.6 MB (65.96%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -2,15 +2,33 @@ class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
         std::sort(nums.begin(), nums.end());
-        int prev = nums[0];
-        int result = 0;
+        // int prev = nums[0];
+        // int result = 0;
+        // for(int i=1; i<nums.size(); i++)
+        // {
+        //     if(prev >= nums[i]) {
+        //         result += (prev - nums[i] + 1);
+        //         prev++;
+        //     }
+        //     else prev = nums[i];
+        // }
+        nums.push_back(nums[nums.size()-1] + nums.size());
+        int result = 0, taken = 0;
+        // int result = 0;
+        int give;
         for(int i=1; i<nums.size(); i++)
         {
-            if(prev >= nums[i]) {
-                result += (prev - nums[i] + 1);
-                prev++;
+            if(nums[i-1] == nums[i])
+            {
+                taken += 1;
+                result -= nums[i];
             }
-            else prev = nums[i];
+            else 
+            {
+                give = min(taken, nums[i]-nums[i-1]-1);
+                result += (give * (give + 1) /2) + (give *nums[i-1]);
+                taken -= give;
+            }
         }
         return result;
     }
