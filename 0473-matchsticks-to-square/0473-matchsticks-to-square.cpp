995d6ecb6045cd01ec69a8fd8eaff259e34a4b4b
Sukesh Cheripalli <sukesh4018@gmail.com>
2022-11-06 14:06:53 -0500

    Time: 420 ms (50.00%), Space: 24.9 MB (12.81%) - LeetHub

--- 0473-matchsticks-to-square/0473-matchsticks-to-square.cpp
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.cpp
@@ -1,24 +1,38 @@
 class Solution {
 public:
-    long long sums[4];
+    long long sums[4] = {0};
     long side_length;
-    // map<long, bool> cache;
+    map<long, bool> cache;
     
-    bool dfs(vector<int>& ms, int index)
+    bool dfs(vector<int>& ms, int index, std::bitset<19> bb, int numsides)
     {   
-        if(index == ms.size()) 
-            return sums[0] == sums[1] && sums[1] == sums[2] && sums[2] == sums[3];
+        if(sums[numsides] == side_length) {
+            bb[numsides + 16] = 1;
+            return dfs(ms, index+1, bb, numsides+1);
+        }
+        if(numsides == 3) return true;
+        
+        if(cache.count((long)bb.to_ulong()) > 0)
+            return cache[(long)bb.to_ulong()];
         
         bool ret = false;
-        for(int i=0; i<4; i++)
+        // for(int i=0; i<4; i++)
+        for(int i=0; i<ms.size(); i++)
         {
-            if(sums[i] + ms[index] > side_length) continue;
-            sums[i] += ms[index];
-            if(dfs(ms, index+1))
-                return true;
-            sums[i] -= ms[index];
+            if(bb[i] == 0) // not taken
+            {
+                if(sums[numsides] + ms[i] > side_length) continue;
+                sums[numsides] += ms[i];
+                bb[i] = 1;
+                ret |= dfs(ms, i+1, bb, numsides);
+                bb[i] = 0;
+                if(ret)
+                    break;
+                sums[numsides] -= ms[i];    
+            }
         }
-        return false;
+        cache[(long)bb.to_ulong()] = ret;
+        return ret;
     }
     
     bool makesquare(vector<int>& matchsticks) {
@@ -29,7 +43,7 @@ public:
         if(sum % 4 != 0) return false;
         side_length = sum/4;
         std::sort(matchsticks.begin(), matchsticks.end(), std::greater<int>());
-        std::bitset<15> bb{0};
-        return dfs(matchsticks, 0);
+        std::bitset<19> bb{0};
+        return dfs(matchsticks, 0, bb, 0);
     }
 };
