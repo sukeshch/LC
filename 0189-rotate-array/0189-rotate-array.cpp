6227daafeb63a2cf24f63b940d9424c62359fc3c
Sukesh Cheripalli <sukesh4018@gmail.com>
2022-11-18 15:14:40 -0500

    Time: 40 ms (81.19%), Space: 24.9 MB (84.62%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    template<class T>
+    void printArr(vector<T> arr)
+    {
+        for(auto v : arr)
+            cout << v << " ";
+        cout << endl;
+    }
+    void rotate(vector<int>& nums, int k) {
+        int count = 0, n = nums.size();
+        k = k % n; // avoid multi rotations
+        int current = 0;
+        while(count < n) {
+            int prev = nums[current], next = 0, ct = 0;
+            while((current + ct * k + k)%n != current) {
+                int temp = nums[(current + ct * k + k)%n];                    
+                nums[(current + ct * k + k)%n] = prev;
+                prev = temp;
+                ct++;
+                count++;
+            }
+            nums[current] = prev;
+            count++;
+            current++;
+        }
+    }
+};
