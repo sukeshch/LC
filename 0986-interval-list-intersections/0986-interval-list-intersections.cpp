26591d4671a590ee6eb27fc866283989132b3f68
Sukesh Cheripalli <sukesh4018@gmail.com>
2022-11-18 00:05:10 -0500

    Time: 83 ms (37.63%), Space: 18.7 MB (43.26%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.cpp
+++ 0986-interval-list-intersections/0986-interval-list-intersections.cpp
@@ -5,13 +5,6 @@ public:
         int counter1 = 0, counter2 = 0;
         while(counter1 < A.size() && counter2 < B.size()) {
             if(min(A[counter1][1], B[counter2][1])-max(A[counter1][0], B[counter2][0]) >= 0) { // no overlap 
-                // if(A[counter1][0] > B[counter2][0]) {
-                //     counter2++;
-                // }
-                // else {
-                //     counter1++;
-                // }
-                // continue;
                 result.push_back({max(A[counter1][0], B[counter2][0]), min(A[counter1][1], B[counter2][1]), });
             }
             if(A[counter1][1] > B[counter2][1])
