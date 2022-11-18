7820b9ba339cd8fcdab75bf11ae19ff4d4075bae
Sukesh Cheripalli <sukesh4018@gmail.com>
2022-11-18 00:02:59 -0500

    Time: 72 ms (58.60%), Space: 18.6 MB (92.45%) - LeetHub

---
+++ 0986-interval-list-intersections/0986-interval-list-intersections.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& A, vector<vector<int>>& B) {
+        vector<vector<int>> result;
+        int counter1 = 0, counter2 = 0;
+        while(counter1 < A.size() && counter2 < B.size())
+        {
+            int diff = min(A[counter1][1], B[counter2][1])-max(A[counter1][0], B[counter2][0]);
+            if(diff < 0) // no overlap
+            {
+                if(A[counter1][0] > B[counter2][0])
+                {
+                    // result.push_back(B[counter2]);
+                    counter2++;
+                }
+                else 
+                {
+                    // result.push_back(A[counter2]);
+                    counter1++;
+                }
+                continue;
+            }
+            result.push_back({max(A[counter1][0], B[counter2][0]), min(A[counter1][1], B[counter2][1]), });
+            if(A[counter1][1] > B[counter2][1])
+            {
+                counter2++;
+            }
+            else counter1++;
+        }
+        
+        return result;
+    }
+};
