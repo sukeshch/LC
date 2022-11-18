4cb9cc2a5f0b59b90f2428a99a18ef7cb1744510
Sukesh Cheripalli <sukesh4018@gmail.com>
2022-11-18 15:26:42 -0500

    Time: 81 ms (34.29%), Space: 24.9 MB (93.72%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -10,19 +10,26 @@ public:
     void rotate(vector<int>& nums, int k) {
         int count = 0, n = nums.size();
         k = k % n; // avoid multi rotations
-        int current = 0;
-        while(count < n) {
-            int prev = nums[current], next = 0, ct = 0;
-            while((current + ct * k + k)%n != current) {
-                int temp = nums[(current + ct * k + k)%n];                    
-                nums[(current + ct * k + k)%n] = prev;
-                prev = temp;
-                ct++;
-                count++;
-            }
-            nums[current] = prev;
-            count++;
-            current++;
-        }
+        // int current = 0;
+        // while(count < n) {
+        //     int prev = nums[current], next = 0, ct = 0;
+        //     while((current + ct * k + k)%n != current) {
+        //         int temp = nums[(current + ct * k + k)%n];                    
+        //         nums[(current + ct * k + k)%n] = prev;
+        //         prev = temp;
+        //         ct++;
+        //         count++;
+        //     }
+        //     nums[current] = prev;
+        //     count++;
+        //     current++;
+        // }
+        for(int i=0; i<n-k; i+=2)
+            swap(nums[i/2], nums[n-1-k-i/2]);
+        // printArr(nums);
+        for(int start = n-k, i=0; i+start<n; i+=2)
+            swap(nums[start + i/2], nums[n-1-i/2]);
+        // printArr(nums);
+        reverse(nums.begin(), nums.end());
     }
 };
