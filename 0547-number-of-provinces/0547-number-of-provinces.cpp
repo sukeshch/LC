2dd5e69c620c79172ff7b7558c79678cf37ed559
Sukesh Cheripalli <sukesh4018@gmail.com>
2022-11-19 17:44:09 -0500

    Time: 79 ms (9.17%), Space: 14.2 MB (42.44%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,27 +1,38 @@
 class Solution {
 public:
-    void DFS(vector<vector<int>>& isConnected, vector<bool>&visited)
-    {
-        
-    }
-    
     int findCircleNum(vector<vector<int>>& isConnected) {
         vector<bool> visited(isConnected.size(), 0);
         int provinces = 0;
         for(int i=0; i<isConnected.size(); i++) {
             if(!visited[i]) {
-                queue<int> st;
-                st.push(i);
+                // BFS
+                /* queue<nt> q;
+                q.push(i);
                 visited[i] = true;
-                while(!st.empty()) {
-                    for(int sz = st.size(); sz>0; sz--) {
-                        int top = st.front();
-                        st.pop();
+                while(!q.empty()) {
+                    for(int sz = q.size(); sz>0; sz--) {
+                        int top = q.front();
+                        q.pop();
                         for(int k=0; k<isConnected.size(); k++) {
                             if(isConnected[top][k] && !visited[k]) {
                                 visited[k] = true;
-                                st.push(k);
+                                q.push(k);
                             }
+                        }
+                    }
+                } */
+                // DFS
+                stack<int> st; 
+                st.push(i);
+                while(!st.empty()) {
+                    int top = st.top();
+                    st.pop();
+                    if(!visited[top]) {
+                        visited[top] = true;
+                    }
+                    for(int i=0; i<isConnected.size(); i++) {
+                        if((i != top) && isConnected[top][i] == 1 && !visited[i]) {
+                            st.push(i);
                         }
                     }
                 }
