b7d8511b79a8b337cc122c14d0bde0a1cd8b53d5
Sukesh Cheripalli <sukesh4018@gmail.com>
2022-11-19 18:17:23 -0500

    Time: 3 ms (73.20%), Space: 9.8 MB (37.73%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -16,6 +16,26 @@ public:
         invertTree(root->left);
         invertTree(root->right);
         std::swap(root->left, root->right);
+        // queue<TreeNode*> q;
+        // q.push(root->left);
+        // q.push(root->right);
+        // while(!q.empty())
+        // {
+        //     auto left = q.front();
+        //     q.pop();
+        //     auto right = q.front();
+        //     q.pop();
+        //     cout << (left ? left->val : -1) << " " << (right? right->val : -1) << endl;
+        //     std::swap(left, right);
+        //     if(left) {
+        //         q.push(left->left);
+        //         q.push(left->right);
+        //     }
+        //     if(right) {
+        //         q.push(right->left);
+        //         q.push(right->right);
+        //     }
+        // }        
         return root;
     }
 };
