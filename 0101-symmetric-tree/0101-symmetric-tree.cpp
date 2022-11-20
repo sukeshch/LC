73bb0ebbfbbef05328bdec55f72a1e9cb2253034
Sukesh Cheripalli <sukesh4018@gmail.com>
2022-11-19 21:17:38 -0500

    Time: 12 ms (37.74%), Space: 16.5 MB (18.50%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -11,33 +11,41 @@
  */
 class Solution {
 public:
+    bool recurse(TreeNode* t1, TreeNode* t2) {
+        if(!t1 && !t2) return true;
+        if(!t1 || !t2) return false;
+        
+        return t1->val == t2->val && recurse(t1->left, t2->right) 
+            && recurse(t1->right, t2->left);
+        // deque<TreeNode*> dq;
+        // dq.push_front(root);
+        // while(!dq.empty()) {
+        //     deque<TreeNode*> tmp;
+        //     for(int i = 0, sz = dq.size(); i<sz; i+=2) {
+        //         if(!dq.front() && !dq.back()) {
+        //             dq.pop_front();
+        //             if(!dq.empty()) dq.pop_back();
+        //             continue;         // both NULL continue
+        //         }
+        //         if(!dq.front() || !dq.back()) return false;       // only one is NULL
+        //         if(dq.front()->val != dq.back()->val)       // values dont match
+        //             return false;
+        //         TreeNode* v = dq.front();
+        //         dq.pop_front();
+        //         tmp.push_front(v->right);
+        //         tmp.push_front(v->left);
+        //         if(!dq.empty()){
+        //             v = dq.back();
+        //             dq.pop_back();
+        //             tmp.push_back(v->left);
+        //             tmp.push_back(v->right);
+        //         }
+        //     }
+        //     dq = tmp;
+        // }
+        // return true;
+    }
     bool isSymmetric(TreeNode* root) {
-        deque<TreeNode*> dq;
-        dq.push_front(root);
-        while(!dq.empty()) {
-            deque<TreeNode*> tmp;
-            for(int i = 0, sz = dq.size(); i<sz; i+=2) {
-                if(!dq.front() && !dq.back()) {
-                    dq.pop_front();
-                    if(!dq.empty()) dq.pop_back();
-                    continue;         // both NULL continue
-                }
-                if(!dq.front() || !dq.back()) return false;       // only one is NULL
-                if(dq.front()->val != dq.back()->val)       // values dont match
-                    return false;
-                TreeNode* v = dq.front();
-                dq.pop_front();
-                tmp.push_front(v->right);
-                tmp.push_front(v->left);
-                if(!dq.empty()){
-                    v = dq.back();
-                    dq.pop_back();
-                    tmp.push_back(v->left);
-                    tmp.push_back(v->right);
-                }
-            }
-            dq = tmp;
-        }
-        return true;
+        return recurse(root, root);
     }
 };
