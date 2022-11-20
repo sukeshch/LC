2498a25d8b525e1e06b03a6b88c48b9df95f1424
Sukesh Cheripalli <sukesh4018@gmail.com>
2022-11-20 16:01:17 -0500

    Time: 38 ms (88.80%), Space: 31.1 MB (8.69%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool searchLeft(TreeNode* root, int depth) {
+        int d = 0;
+        while(root) {
+            root = root->left;
+            d++;
+        }
+        if(d == depth) // right exists
+            return true;
+        return false;
+    }
+    bool searchRight(TreeNode* root, int depth) {
+        int d = 0;
+        while(root) {
+            root = root->right;
+            d++;
+        }
+        if(d == depth) // right exists
+            return true;
+        return false;
+    }
+    
+    int recurse(TreeNode* root, int depth) {
+        cout << ((root) ? root->val : 00) << " :@ " << depth << endl;
+        if(!root && depth >= 1) return -1;
+        if(depth == 1) return 1;
+        if(!searchLeft(root, depth))
+            return -1;
+        if(searchRight(root, depth)) 
+            return int(pow(2, depth))-1;
+        int ans;
+        ans = recurse(root->right, depth-1);
+        if(ans != -1) 
+            return ans + int(pow(2, depth-1));
+        return pow(2, depth-2) + recurse(root->left, depth-1);
+    }
+    int countNodes(TreeNode* root) {
+        if(!root) return 0;
+        int max_depth = 0;
+        TreeNode* left = root;
+        while(left) {
+            left = left->left;
+            max_depth++;
+        }
+        cout << "max depth : " << max_depth << endl;
+        return recurse(root, max_depth);
+        // return 1;
+    }
+};
