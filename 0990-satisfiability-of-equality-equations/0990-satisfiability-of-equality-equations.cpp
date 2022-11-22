46f05851b0c5fca6b4c6522d92392d926a49a6de
Sukesh Cheripalli <sukesh4018@gmail.com>
2022-11-22 14:54:57 -0500

    Time: 11 ms (60.48%), Space: 11.4 MB (27.45%) - LeetHub

--- 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.cpp
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool equationsPossible(vector<string>& equations) {
-        vector<int> map(26, -1);
+        /* vector<int> map(26, -1);
         vector<set<char>> v;
         for(auto eq : equations) {
             if(eq[1] == '=') {
@@ -32,6 +32,43 @@ public:
             if(eq[1] == '!') {
                 char x = eq[0], y = eq[3];
                 if(x == y || (map[int(x-'a')] != -1 && map[int(x-'a')] == map[int(y-'a')])) {
+                    return false;
+                }
+            }
+        } */
+        vector<char> map(26);
+        vector<bool> b(26);
+        for(int i=0; i<26; i++) {
+            map[i] = ('a' + i);
+        }
+        for(auto eq : equations) {
+            if(eq[1] == '=') {
+                char x = eq[0], y = eq[3];
+                if(b[int(x-'a')] && b[int(y-'a')]) {
+                    char t = map[int(y-'a')];
+                    for(int i=0; i<26; i++) {
+                        if(map[i] == t) {
+                            // set parent to x if parent is y
+                            map[i] = map[int(x-'a')];
+                        }
+                    }
+                } else if (b[int(x-'a')] && !b[int(y-'a')]) {
+                    map[int(y-'a')] = map[int(x-'a')];
+                    b[int(y-'a')] = true;
+                } else if (!b[int(x-'a')] && b[int(y-'a')]) {
+                    map[int(x-'a')] = map[int(y-'a')];
+                    b[int(x-'a')] = true;
+                } else {
+                    map[int(y-'a')] = map[int(x-'a')];
+                    b[int(x-'a')] = true;
+                    b[int(y-'a')] = true;
+                }
+            } 
+        }
+        for(auto eq : equations) {
+            if(eq[1] == '!') {
+                char x = eq[0], y = eq[3];
+                if(x == y || map[int(x-'a')] == map[int(y-'a')]) {
                     return false;
                 }
             }
