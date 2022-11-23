d0b9892367ecff8be538a72133e29b871c1fb165
Sukesh Cheripalli <sukesh4018@gmail.com>
2022-11-23 06:00:15 -0500

    Time: 228 ms (48.53%), Space: 134 MB (24.00%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,54 +1,55 @@
 class Solution {
 public:
     class Node {
+    private:
+        vector<Node*> childs_;
+        vector<string> data_;
+        string current_;
     public:
         Node(string c = " ") : current_(c) {
             childs_.resize(26, NULL);
         }
-        vector<Node*> childs_;
-        vector<string> data_;
-        string current_;
+        void insert(Node* t, string str) {
+            int index = str[0]-'a';
+
+            Node *croot = t;
+            for(int i=0; i<str.size(); i++) {
+                index = str[i] - 'a';
+                if(!croot->childs_[index]) {
+                    croot->childs_[index] = new Node(str.substr(0, i+1));
+                }
+                croot = croot->childs_[index];
+
+                if(croot->data_.size() < 3) {
+                    croot->data_.push_back(str);
+                }
+            }
+
+        }
+        vector<vector<string>> search(Node* t, string word) {
+            vector<vector<string>> result;
+
+            int index = word[0]-'a';
+            Node *croot = t;
+
+            for(int i=0; i<word.size(); i++) {
+                index = word[i] - 'a';
+                if(!croot->childs_[index]) {
+                    while(i < word.size()) {
+                        result.push_back({});
+                        i++;
+                    }
+                    break;
+                }
+                croot = croot->childs_[index];
+                result.push_back(croot->data_);
+            }
+
+            return result;
+        }
     } root;
     
-    void insert(string str) {
-        int index = str[0]-'a';
-        Node *croot = &root;
-        
-        for(int i=0; i<str.size(); i++) {
-            index = str[i] - 'a';
-            if(!croot->childs_[index]) {
-                croot->childs_[index] = new Node(str.substr(0, i+1));
-            }
-            croot = croot->childs_[index];
-            
-            if(croot->data_.size() < 3) {
-                croot->data_.push_back(str);
-            }
-        }
-        
-    }
     
-    vector<vector<string>> search(string word) {
-        vector<vector<string>> result;
-        
-        int index = word[0]-'a';
-        Node *croot = &root;
-        
-        for(int i=0; i<word.size(); i++) {
-            index = word[i] - 'a';
-            if(!croot->childs_[index]) {
-                while(i < word.size()) {
-                    result.push_back({});
-                    i++;
-                }
-                break;
-            }
-            croot = croot->childs_[index];
-            result.push_back(croot->data_);
-        }
-        
-        return result;
-    }
     
     vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
         vector<string> cpy_prod(products);
@@ -56,9 +57,9 @@ public:
         
         // Build Trie
         for(auto prod : cpy_prod) {
-            insert(prod);
+            root.insert(&root, prod);
         }
         
-        return search(searchWord);
+        return root.search(&root, searchWord);
     }
 };
