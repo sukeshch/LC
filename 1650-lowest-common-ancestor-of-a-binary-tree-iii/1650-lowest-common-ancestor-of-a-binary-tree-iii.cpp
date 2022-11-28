734349bf9976fe67fc7b2d98e0a607db537e0574
Sukesh Cheripalli <sukesh4018@gmail.com>
2022-11-28 02:51:08 -0500

    Time: 34 ms (33.63%), Space: 10.1 MB (86.90%) - LeetHub

---
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.cpp
@@ -0,0 +1,34 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* parent;
+};
+*/
+
+class Solution {
+public:
+    Node* lowestCommonAncestor(Node* p, Node * q) {
+        unordered_map<int, Node*> mp_;
+        mp_[p->val] = p;
+        mp_[q->val] = q;
+        Node* pp = p->parent, *pq = q->parent;
+        while(pp || pq) {
+            // cout << (pp ? pp->val : -1) << " " << (pq ? pq->val : -1) << endl;;
+            if(pp) {
+                if(mp_.count(pp->val) != 0) return mp_[pp->val];
+                mp_[pp->val] = pp;
+                pp = pp->parent;
+            }
+            if(pq) {
+                if(mp_.count(pq->val) != 0) return mp_[pq->val];
+                mp_[pq->val] = pq;
+                pq = pq->parent;
+            }
+        }
+        return pp;
+    }
+};
