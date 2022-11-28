8644ef1f18ed32682072b11298dd65551bb1dd18
Sukesh Cheripalli <sukesh4018@gmail.com>
2022-11-28 02:26:46 -0500

    Time: 27 ms (98.02%), Space: 23.1 MB (93.27%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -14,21 +14,15 @@ public:
         int min = root->val, max = root->val;
         int smin = std::min(p->val, q->val);
         int smax = std::max(p->val, q->val);
-        while(true) {
-            if(!root) break;
-            if(root->val != p->val && root->val != q->val) {
-                if(smin < root->val && root->val < smax) {
-                    return root;
-                }
-                if(smin < root->val && smax < root->val) {
-                    root = root->left;
-                } else {
-                    root = root->right;
-                }
+        while(root) {
+            if(smin < root->val && smax < root->val) {
+                root = root->left;
+            } else if(smin > root->val && smax > root->val) {
+                root = root->right;
             }
-            else 
+            else {
                 return root;
-            
+            }
         }
         return root;
     }
