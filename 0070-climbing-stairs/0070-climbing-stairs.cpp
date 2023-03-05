01cc12c6d3e6a914531a54e39687b94a81db8263
Sukesh Cheripalli <12154380+sukeshch@users.noreply.github.com>
2023-03-05 14:02:38 -0500

    Time: 0 ms (100.00%), Space: 6 MB (45.52%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -8,12 +8,16 @@ public:
     }
     
     int climbStairs(int n) {
-        vector<int> res(n + 1, 0);
-        res[0] = 1;
-        res[1] = 1;
+        // vector<int> res(n + 1, 0);
+        // res[0] = 1;
+        // res[1] = 1;
+        if(n==1) return 1;
+        int f = 1, s = 1, res = 0;
         for(int i = 2; i <= n; i++) {
-            res[i] = res[i-1] + res[i-2];
+            res = f + s;
+            s = f; 
+            f = res;
         }
-        return res[n];
+        return res;
     }
 };
