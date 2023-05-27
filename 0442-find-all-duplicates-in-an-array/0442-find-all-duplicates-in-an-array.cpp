27f7dc1046bc2a5630a26e0ce5feb99c7be3b44e
Sukesh Cheripalli <12154380+sukeshch@users.noreply.github.com>
2023-05-27 15:25:13 -0400

    Time: 59 ms (81.77%), Space: 33.6 MB (77.93%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -7,8 +7,8 @@ public:
         {
             if(nums[abs(num)-1] < 0)
                 result.push_back(abs(num));
-            else 
-                nums[abs(num)-1]  = -1 * nums[abs(num)-1];
+             
+            nums[abs(num)-1]  = -1 * nums[abs(num)-1];
         }
         return result;
     }
