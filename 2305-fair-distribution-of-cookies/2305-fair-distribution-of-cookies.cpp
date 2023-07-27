a43134458ec0bf02fc02d890432800d1ec5d043a
Sukesh Cheripalli <12154380+sukeshch@users.noreply.github.com>
2023-07-27 03:59:34 -0400

    Time: 83 ms (79.11%), Space: 6.9 MB (48.71%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
-    int recurse(vector<int>& cookies, int index, vector<int> &distribute) {
+    int recurse(vector<int>& cookies, int index, vector<int> &distribute, int zero_count) {
+        if(zero_count > cookies.size() - index)
+            return INT_MAX;
+        
         if(index == cookies.size()) {
             return *max_element(distribute.begin(), distribute.end());
         }
         int minv = INT_MAX;
         for(int i=0; i<distribute.size(); i++) {
+            if(!distribute[i]) zero_count--;
             distribute[i] += cookies[index];
-            minv = min(minv, recurse(cookies, index + 1, distribute));
+            minv = min(minv, recurse(cookies, index + 1, distribute, zero_count));
             distribute[i] -= cookies[index];
+            if(!distribute[i]) zero_count++;
         }
         return minv;
     }
     
     int distributeCookies(vector<int>& cookies, int k) {
         vector<int> distribute(k, 0);
-        
-        return recurse(cookies, 0, distribute);
+        int zero_count = k;
+        return recurse(cookies, 0, distribute, zero_count);
     }
 };
