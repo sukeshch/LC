7665d96dfd8a0c168cb778e16499cf1484182dfd
Sukesh Cheripalli <12154380+sukeshch@users.noreply.github.com>
2023-08-30 12:21:25 -0700

    Time: 22 ms (85.47%), Space: 11 MB (67.96%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -3,28 +3,23 @@ public:
     int bestClosingTime(string customers) {
         int N = customers.size();
         vector<bool> is_customer(N, false);
+        int total_penalty = 0;
         for(int i=0; i<N; i++) {
-            if(customers[i] == 'Y') 
+            if(customers[i] == 'Y')  {
                 is_customer[i] = true;
+                total_penalty++;
+            }
         }
-        
-        vector<int> penalty_open(N+2,0);
-        vector<int> penalty_close(N+2,0);
-        
-        int res_ind = -1, min_penalty = INT_MAX;
-        penalty_open[0] = 0;
-        penalty_close[N+1] = 0;
+        int res_ind = 0, min_penalty = total_penalty;
+        int cur_penalty = total_penalty;
         
         for ( int i = 0; i < N; i++ ) {
-            penalty_open[i + 1] = penalty_open[i] + (is_customer[i] ? 0 : 1);
-            penalty_close[N-1-i+1] = 
-                penalty_close[N-1-i+1 + 1] + (is_customer[N-1-i] ? 1 : 0);
-        }
-        
-        for(int i=0; i<=N; i++) {
-            if(min_penalty > (penalty_open[i] + penalty_close[i+1]))  {
-                min_penalty = penalty_open[i] + penalty_close[i+1];
-                res_ind = i;
+            
+            cur_penalty += is_customer[i] ? -1 : 1;
+                
+            if(cur_penalty < min_penalty) {
+                res_ind = i+1;
+                min_penalty = cur_penalty;
             }
         }
         
