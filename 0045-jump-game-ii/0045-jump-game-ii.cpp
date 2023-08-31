031e17b615b1e7e647390550f4cac76b43a74d1e
Sukesh Cheripalli <12154380+sukeshch@users.noreply.github.com>
2023-08-30 22:37:54 -0700

    Time: 331 ms (29.12%), Space: 18.1 MB (5.01%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int recurse(vector<int>& nums, int index, vector<int> &dp) {
+        if(dp[index] != -1) {
+            return dp[index];
+        }
+        if(index == nums.size() - 1) {
+            return 0;
+        }
+        if(nums[index] == 0) {
+            dp[index] = 10000;
+        }
+        if(index + nums[index] >= nums.size() - 1)
+        {
+            return 1;
+        }
+        int min_st = 10001;
+        for(int i = 1; i <= nums[index] && (index + i < nums.size()); i++) {
+            int res = recurse(nums, index + i, dp);
+            if(res < min_st) {
+                min_st = res;
+            }
+        }
+        // std::cout << "index : " << index << endl;
+        return dp[index] = min_st + 1;
+    }
+    
+    int jump(vector<int>& nums) {
+        vector<int> dp(nums.size(), -1);
+        return recurse(nums, 0, dp);
+    }
+};
