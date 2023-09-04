a0f3fa6782147e9ec9601d2c4f8431229a3f3288
Sukesh Cheripalli <12154380+sukeshch@users.noreply.github.com>
2023-09-04 19:14:46 -0400

    Time: 51 ms (64.98%), Space: 24.5 MB (51.79%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -24,12 +24,25 @@ public:
         }
         return dp[id1][id2] = length;
     }
+    // recursive
+    // int longestCommonSubsequence(string text1, string text2) {
+    //     if(text1 == text2) return text1.size();
+    //     if(text1 == "" || text2 == "") return 0;
+    //     dp = std::vector<std::vector<int>>(
+    //                 text1.size(), std::vector<int>(text2.size(), -1));
+    //     return recurse(text1, text2, 0, 0);
+    // }
     
     int longestCommonSubsequence(string text1, string text2) {
-        if(text1 == text2) return text1.size();
-        if(text1 == "" || text2 == "") return 0;
         dp = std::vector<std::vector<int>>(
-                    text1.size(), std::vector<int>(text2.size(), -1));
-        return recurse(text1, text2, 0, 0);
+                    text1.size() + 1, std::vector<int>(text2.size() + 1, 0));
+        for(int i=0; i<text1.size(); i++) {
+            for(int j=0; j<text2.size(); j++) {
+                // cout << i << "x" << j << endl;
+                dp[i+1][j+1] = 
+                    (text1[i] == text2[j]) ? 1 + dp[i][j] : max(dp[i+1][j], dp[i][j+1]);
+            }
+        }
+        return dp[text1.size()][text2.size()];
     }
 };
