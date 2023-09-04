1061480ff05f3fe657ec7287fd3e63a1aee6ae96
Sukesh Cheripalli <12154380+sukeshch@users.noreply.github.com>
2023-09-04 19:50:23 -0400

    Time: 108 ms (63.40%), Space: 387.3 MB (13.03%) - LeetHub

---
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int recurse(string &s, int st, int end) {
+        if(dp[st][end] != 0) return dp[st][end];
+        if(st == end) return 1;
+        if(st > end) return 0;
+        int res;
+        if(s[st] == s[end]) {
+            res =  2 + recurse(s, st+1, end-1);
+        } else {
+            res = max(
+                recurse(s, st, end-1), 
+                recurse(s, st+1, end) 
+                      );
+        }
+        return dp[st][end] = res;
+    }
+    
+    int longestPalindromeSubseq(string s) {
+        dp = std::vector<vector<int>>(1001, std::vector<int>(1001, 0));
+        return recurse(s, 0, s.size()-1);
+    }
+};
