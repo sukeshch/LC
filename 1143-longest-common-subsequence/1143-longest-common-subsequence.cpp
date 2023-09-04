b59bfce22d7dfb6d8edad2cf98768896b1282eaf
Sukesh Cheripalli <12154380+sukeshch@users.noreply.github.com>
2023-09-04 19:28:28 -0400

    Time: 51 ms (64.98%), Space: 6.7 MB (88.91%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int recurse(string &text1, string &text2, int id1, int id2) {
-        if(id1 >= text1.size() || id2 >= text2.size()) {
-            return 0;
-        }
-        if(dp[id1][id2] != -1) {
-            // cout << "memo found \n";
-            return dp[id1][id2];
-        }
-        int length = -1; // max
-        if(text1[id1] == text2[id2]) {
-            length = max(length,  
-                        1 + recurse(text1, text2, id1 + 1, id2 + 1)
-                        );
-        } else {
-            // not matching text2[i]
-            length = max(length,  
-                            recurse(text1, text2, id1, id2 + 1));
-            length = max(length,  
-                            recurse(text1, text2, id1+1, id2));
+    // vector<vector<int>> dp;
+//     int recurse(string &text1, string &text2, int id1, int id2) {
+//         if(id1 >= text1.size() || id2 >= text2.size()) {
+//             return 0;
+//         }
+//         if(dp[id1][id2] != -1) {
+//             // cout << "memo found \n";
+//             return dp[id1][id2];
+//         }
+//         int length = -1; // max
+//         if(text1[id1] == text2[id2]) {
+//             length = max(length,  
+//                         1 + recurse(text1, text2, id1 + 1, id2 + 1)
+//                         );
+//         } else {
+//             // not matching text2[i]
+//             length = max(length,  
+//                             recurse(text1, text2, id1, id2 + 1));
+//             length = max(length,  
+//                             recurse(text1, text2, id1+1, id2));
             
-        }
-        return dp[id1][id2] = length;
-    }
+//         }
+//         return dp[id1][id2] = length;
+//     }
     // recursive
     // int longestCommonSubsequence(string text1, string text2) {
     //     if(text1 == text2) return text1.size();
@@ -34,15 +34,16 @@ public:
     // }
     
     int longestCommonSubsequence(string text1, string text2) {
-        dp = std::vector<std::vector<int>>(
-                    text1.size() + 1, std::vector<int>(text2.size() + 1, 0));
+        auto dp = std::vector<std::vector<int>>(2, std::vector<int>(text2.size() + 1, 0));
+        int cur = 1, prev = 0;
         for(int i=0; i<text1.size(); i++) {
             for(int j=0; j<text2.size(); j++) {
-                // cout << i << "x" << j << endl;
-                dp[i+1][j+1] = 
-                    (text1[i] == text2[j]) ? 1 + dp[i][j] : max(dp[i+1][j], dp[i][j+1]);
+                dp[cur][j+1] = 
+                    (text1[i] == text2[j]) ? 1 + dp[prev][j] : max(dp[prev][j+1], dp[cur][j]);
             }
+            cur ^= 1;
+            prev ^= 1;
         }
-        return dp[text1.size()][text2.size()];
+        return dp[prev][text2.size()];
     }
 };
