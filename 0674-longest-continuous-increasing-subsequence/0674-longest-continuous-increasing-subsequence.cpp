737e65b35ddf354529b16c23c5e4771be10993b4
Sukesh Cheripalli <12154380+sukeshch@users.noreply.github.com>
2023-09-05 14:25:35 -0400

    Time: 7 ms (79.53%), Space: 11 MB (41.90%) - LeetHub

---
+++ 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findLengthOfLCIS(vector<int>& nums) {
+        int start = 0, curr = start + 1;
+        int lcis = 1;
+        while(curr < nums.size()) {
+            if(nums[curr] > nums[curr-1]) {
+                // cout << "curr " << curr << endl;
+                lcis = max(lcis, curr - start + 1);
+            } else {
+                start = curr;
+            }
+            curr++;
+        }
+        lcis = max(lcis, curr - start);
+        return lcis;
+    }
+};
