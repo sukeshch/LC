4a5a28478913d16475ae91a12b7314dcf0e02576
Sukesh Cheripalli <12154380+sukeshch@users.noreply.github.com>
2023-09-08 12:28:19 -0400

    Time: 60 ms (47.59%), Space: 48.5 MB (27.30%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
+    // bool canJump(vector<int>& nums) {
+    //     // DP - O(n^2)
+    //     int N = nums.size();
+    //     vector<bool> dp = vector<bool>(nums.size() + 1, 0);
+    //     dp[N-1] = true;
+    //     for(int i = N-2; i >=0 ; i--) {
+    //         bool canreach = false;
+    //         for(int j = i; j <= i + nums[i] && i < N; j++) {
+    //             canreach = dp[j];
+    //             if(canreach)
+    //                 break;
+    //         }
+    //         dp[i] = canreach;
+    //     }
+    //     return dp[0];
+    // }
+    
     bool canJump(vector<int>& nums) {
+        int cur_max = 0;
         int N = nums.size();
-        vector<bool> dp = vector<bool>(nums.size() + 1, 0);
-        dp[N-1] = true;
-        for(int i = N-2; i >=0 ; i--) {
-            bool canreach = false;
-            for(int j = i; j <= i + nums[i] && i < N; j++) {
-                canreach = dp[j];
-                if(canreach)
-                    break;
-            }
-            dp[i] = canreach;
+        int i = 0;
+        while(i < N) {
+            if(i > cur_max)
+                return false;
+            
+            cur_max = max(cur_max, i + nums[i]);
+            
+            if(cur_max == N-1) 
+                return true;
+            i++;
         }
-        return dp[0];
+        
+        return (cur_max >= (nums.size()-1));
     }
 };
