df631f22d41306a1835030bc0e06a6890fa8037c
Sukesh Cheripalli <12154380+sukeshch@users.noreply.github.com>
2023-09-08 21:10:13 -0400

    Time: 0 ms (100.00%), Space: 7.9 MB (22.09%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,25 +1,12 @@
 class Solution {
 public:
-    int recurse(vector<int>& nums, int index, vector<int>& sum) {
-        if(index >= nums.size()) {
-            return 0;
-        }
-        sum[index] = max(
-            nums[index] + (sum[index+2] == -1 ? recurse(nums, index+2, sum) : sum[index+2]),
-            (sum[index+1] == -1 ? recurse(nums, index+1, sum) : sum[index+1])
-        );
-        return sum[index];
-    }
-    
     int rob(vector<int>& nums) {
-        if(nums.empty()) return 0;
-        vector<int> sum(nums.size() + 2, 0);
-        // return recurse(nums, 0, sum);
-        sum[nums.size()] = 0;
-        sum[nums.size()-1] = nums.back();
-        for(int i=nums.size()-2; i>=0; i--) {
-            sum[i] = max(sum[i+1], nums[i] + sum[i+2]);
+        vector<int> dp(nums.size() + 1, 0);
+        dp[nums.size()-1] = nums.back();
+        for (int i=nums.size()-2; i>=0; i--) {
+            dp[i] = max(dp[i+2] + nums[i], dp[i+1]);
         }
-        return sum[0];
+        
+        return dp[0];
     }
 };
