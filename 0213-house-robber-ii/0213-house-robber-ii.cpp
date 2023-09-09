dfee83352f124c26f02e414a6aaf848bb3872d3a
Sukesh Cheripalli <12154380+sukeshch@users.noreply.github.com>
2023-09-08 22:01:15 -0400

    Time: 0 ms (100.00%), Space: 8 MB (14.62%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
-    int recurse(vector<int>& nums, vector<vector<int>>& sum, int index, bool first = false) {
-        // cout << "index " << index << " " << (first ? "true " : " false ") ;
-        if(index >= nums.size()) {
-            // cout << endl;
-            return 0;
+    int rob1(vector<int>& nums, int start, int sz) {
+        vector<int> dp(sz+1, 0);
+        dp[sz-1] = nums[sz-1];
+        for(int i=sz-2; i>=start; i--) {
+            dp[i] = max(dp[i+1], dp[i+2] + nums[i]);
         }
-        if(index == nums.size()-1 && first) {   
-            // cout << endl;
-            return 0;
-        }
-        
-        sum[index][int(first)] = max(
-            nums[index] + ((sum[index+2][int(first)] == -1) ? recurse(nums,sum, index+2, first) : sum[index+2][int(first)]),
-            (sum[index+1][int(first)] == -1) ? recurse(nums, sum, index + 1, first) : sum[index+1][int(first)]
-        );
-        // cout << "sum " << sum[index] << endl;
-        return sum[index][int(first)];
+        return dp[start];
     }
     
     int rob(vector<int>& nums) {
-        vector<vector<int>> sum(nums.size() + 2, {-1, -1});
+        if(nums.size() == 1) return nums[0];
+        if(nums.size() == 2) return max(nums[0], nums[1]);
         return max(
-            recurse(nums, sum, 2, true) + nums[0],
-            recurse(nums, sum, 1, false)
-        );
+            rob1(nums, 1, nums.size()),
+            rob1(nums, 2, nums.size()-1) + nums[0]
+                  );
     }
 };
