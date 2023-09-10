eaa06e8d465ee69add05f2bf2eb707a5703e871a
Sukesh Cheripalli <12154380+sukeshch@users.noreply.github.com>
2023-09-10 01:23:02 -0400

    Time: 0 ms (100.00%), Space: 6.2 MB (19.83%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
-    int recurse(int n) {
-        if(n < 0) return 0;
-        if(n==0)
-            return 1;
-        return recurse(n-1) + recurse(n-2);
-    }
-    
     int climbStairs(int n) {
-        // vector<int> res(n + 1, 0);
-        // res[0] = 1;
-        // res[1] = 1;
-        if(n==1) return 1;
-        int f = 1, s = 1, res = 0;
-        for(int i = 2; i <= n; i++) {
-            res = f + s;
-            s = f; 
-            f = res;
+        vector<int> dp(n+1, 0);
+        dp[n-1] = 1;
+        dp[n] = 1;
+        // 1 --> n
+        for(int i=n-2; i>=0; i--) {
+            dp[i] = dp[i+1] + dp[i+2];
         }
-        return res;
+        return dp[0];
     }
 };
