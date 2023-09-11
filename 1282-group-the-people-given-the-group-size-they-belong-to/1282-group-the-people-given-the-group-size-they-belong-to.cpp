2910aa65bfe5215cf996fa59985470bbcb4adc06
Sukesh Cheripalli <12154380+sukeshch@users.noreply.github.com>
2023-09-10 23:45:27 -0400

    Time: 13 ms (41.63%), Space: 12.5 MB (88.22%) - LeetHub

---
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+        vector<vector<int>> result;
+        unordered_map<int, vector<int>> grp_counts;
+        for(int i=0; i< groupSizes.size(); i++) {
+            grp_counts[groupSizes[i]].push_back(i);
+            if(grp_counts[groupSizes[i]].size() == groupSizes[i]) {
+                result.push_back(grp_counts[groupSizes[i]]);
+                grp_counts[groupSizes[i]] = {};
+            }
+        }
+        
+        return result;
+    }
+};
