379c9f945a2a257f54fc45e67462d8bc3b6233d7
Sukesh Cheripalli <12154380+sukeshch@users.noreply.github.com>
2023-09-11 12:33:32 -0400

    Time: 15 ms (90.52%), Space: 13.5 MB (43.34%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,45 +10,30 @@
  */
 class Solution {
 public:
-    ListNode* Merge2(ListNode* l, ListNode* r)
-    {
-        ListNode* root = new ListNode(-1);
-        
-        ListNode *prev = root;
-        while(l && r)
-        {
-            if(l->val < r->val)
-            {
-                prev->next = l;
-                l = l->next;
-            }
-            else
-            {
-                prev->next = r;
-                r = r->next;
-            }
-            prev = prev->next;
-        }
-        prev->next = (l) ? l : r;
-        return root->next;
-    }
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
     
-    ListNode* mergeKLists(vector<ListNode*>& lists) 
-    {
-        if(lists.size() == 0) return NULL;
-        // for(int i=0; i<lists.size(); i++)
-        // {
-        //     lists[0] = Merge2(lists[0], lists[i]);
-        // }
-        int len = 1;
-        while(len < lists.size())
-        {
-            for(int i=0; (i+len) < lists.size(); i+=2*len)
-            {
-                lists[i] = Merge2(lists[i], lists[i+len]);
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        vector<ListNode*> cur_indexes(lists.size());
+        
+        for(int i=0; i<lists.size(); i++) {
+            cur_indexes[i] = lists[i];
+            if(cur_indexes[i]) {
+                pq.push({cur_indexes[i]->val, i});
             }
-            len*=2;
         }
-        return lists[0];
+        ListNode* result = new ListNode();
+        ListNode *curr = result;
+        while (!pq.empty()) {
+            auto t = pq.top();
+            pq.pop();
+            int l_idx = t.second;
+            curr->next = cur_indexes[l_idx];
+            curr = curr->next;
+            cur_indexes[l_idx] = cur_indexes[l_idx]->next;
+            if(cur_indexes[l_idx]) 
+                pq.push({ cur_indexes[l_idx]->val, l_idx });
+        }
+        
+        return result->next;
     }
 };
