2f4995fdc1605e705b79b03574171fbe764bf164
Sukesh Cheripalli <12154380+sukeshch@users.noreply.github.com>
2023-09-13 15:31:44 -0400

    Time: 31 ms (31.99%), Space: 8 MB (39.16%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,40 +1,57 @@
 class Solution {
 public:
-    bool match(int first[], int second[], int len)
-    {
-        for(int i=0; i<len; i++)
-        {
-            if(first[i] == 0) continue;
-            if(first[i] > second[i]) return false;
+    bool isSubString(int curr[], int target[]) {
+        for(int i=0; i<60; i++) {
+            if(target[i] != 0 && target[i] > curr[i])
+                return false;
         }
         return true;
-    }
-
+    } 
+    
     string minWindow(string s, string t) {
-        if(t.size() > s.size()) return "";
-        int rl = 0, rr = 0, minl = s.size()+100;
-        int l = 0, r = 0;
-        int tcount[60]{0};
-        for(auto c : t)
-            tcount[c-'A']++;
-        int scount[60]{0};
-        while(r < s.size())
-        {
-            while(r < s.size() && !match(tcount, scount, 60))
-            {
-                scount[s[r]-'A']++;
-                r++;
+        int t_count[60]{0};
+        {   
+            int s_count[60]{0};
+            for(auto c : t) {
+                s_count[c-'A']++;
             }
-            while(match(tcount, scount, 60))
-            {
-                if(minl > (r-l))
-                    rl = l, rr = r, minl = (r-l);
-                scount[s[l]-'A']--;
-                l++;
+            for(auto c : t) {
+                t_count[c-'A']++;
+                if(t_count[c-'A'] > s_count[c-'A']) {
+                    return ""; // edge case if s do not contain all chars in t
+                }
             }
         }
-        if(minl == 0 || minl > s.size()) return "";
-        string result = s.substr(rl, minl);
-        return result;
+        
+        int start = 0, end = 0;
+        int m = s.size();
+        int min_len = s.size();
+        int res[2]{0};
+        
+        int curr_count[60]{0};
+        // cout << "s size " << m << endl;
+        while(start < m || end < m) {
+            // if(start > m-500){
+            //     cout << "st : " << start << " end: " << end << endl;
+            // }
+            if(isSubString(curr_count, t_count)) {
+                // cout << "issubstr " << min_len << endl;
+                if(min_len >= (end-start)) {
+                    min_len = end-start;
+                    res[0] = start;
+                    res[1] = end;
+                }
+                curr_count[s[start]-'A']--;
+                start++;
+            }
+            else {
+                // cout << "! issubstr" << endl;
+                if(end == m) break;
+                curr_count[s[end]-'A']++;
+                end++;
+            }
+        }
+        
+        return s.substr(res[0], res[1]-res[0]);
     }
 };
