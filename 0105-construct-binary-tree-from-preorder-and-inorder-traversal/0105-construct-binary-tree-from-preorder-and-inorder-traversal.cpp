98673bc76b140a20190f6e4c145b91025ed0b548
Sukesh Cheripalli <12154380+sukeshch@users.noreply.github.com>
2023-09-13 19:07:46 -0400

    Time: 11 ms (85.77%), Space: 26.1 MB (64.37%) - LeetHub

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    TreeNode *root;
+    
+    TreeNode* recurse(vector<int>& preorder, vector<int>& inorder,
+        int pre_st, int pre_end, int in_st, int in_end) {
+        if(pre_st > pre_end || in_st > in_end)
+            return NULL;
+        if(pre_st == pre_end) {
+            return new TreeNode(preorder[pre_st]);
+        }
+        TreeNode *node = new TreeNode(preorder[pre_st]);
+        
+        auto it = find(inorder.begin() + in_st, inorder.begin() + in_end, node->val);
+        int in_index = it - inorder.begin();
+        
+        // left exists - elements presents between start and 
+        // node->val 
+        int pre_index = pre_st;
+        
+        if(in_index > in_st) { 
+            pre_index = pre_st + (in_index - in_st);
+            node->left = recurse(preorder, inorder, 
+                                pre_st + 1, pre_index,
+                                in_st, in_index-1);
+        }
+        
+        // right exists 
+        if (in_end > in_index) {
+            node->right = recurse(preorder, inorder, 
+                                pre_index + 1, pre_end,
+                                in_index + 1, in_end);
+        }
+        
+        return node;
+    }
+    
+    
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        root = recurse(
+            preorder, inorder, 
+            0, preorder.size()-1, 
+            0, inorder.size()-1
+        );
+        return root;
+    }
+};
