26316b25e99a3accbf2a5cd55e78229fcea8c1c4
Sukesh Cheripalli <12154380+sukeshch@users.noreply.github.com>
2023-09-14 18:04:12 -0400

    Time: 2528 ms (5.01%), Space: 560.8 MB (5.15%) - LeetHub

---
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int result = 0;
+        sort(intervals.begin(), intervals.end(), [](auto a, auto b){
+            return a[1] == b[1] ? a[0] < b[0] : a[1] < b[1];
+            });
+        // for(auto i : intervals) {
+        //     printf("[%d, %d] ", i[0], i[1]);
+        // }
+        
+        int max_end = INT_MIN; // max end seen and valid till now
+        for(int i = 0; i < intervals.size(); i++) {
+            int c[2] = {intervals[i][0], intervals[i][1]}; // curr interval
+            if(
+                c[0] < max_end
+            ) { // overlap
+                result++;
+            } 
+            else {
+                max_end = max(max_end, c[1]);
+            }
+        }
+        
+        return result;
+    }
+};
