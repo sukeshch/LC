71e79ec041a85251924ecf044e0fe1da97821a7b
Sukesh Cheripalli <12154380+sukeshch@users.noreply.github.com>
2023-09-14 21:30:44 -0400

    Time: 11 ms (80.06%), Space: 14 MB (18.04%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,41 +1,22 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        
-        // counts - O( N logN )
-        vector<int> counts;
-        unordered_map<int, int> mp;
-        for(auto num : nums)
-        {
-            mp[num]++;
+        unordered_map<int,int> counts;
+        for(auto n : nums)
+            counts[n]++;
+        priority_queue<pair<int,int>> pq;
+        for(auto p : counts) {
+            pq.push({-p.second, p.first});
+            if(pq.size() > k)
+                pq.pop();
         }
-        for(auto v : mp)
-        {
-            counts.push_back(v.second);
+        vector<int> ans;
+        pair<int,int> top;
+        while(!pq.empty()) {
+            top = pq.top();
+            ans.push_back(top.second);
+            pq.pop();
         }
-        std::sort(counts.begin(), counts.end());
-        int minCount = counts[counts.size()-k];
-        vector<int> result;
-        for(auto v : mp)
-        {
-            if(v.second >= minCount) result.push_back(v.first);
-        }
-        return result;
-        
-        // unordered_map<int,int> cnts_map;
-        // for(auto num : nums)
-        //     cnts_map[num]++;
-        // auto comp = [&cnts_map](int n1, int n2) {return cnts_map[n1] < cnts_map[n2];};
-        // priority_queue<int, vector<int>, decltype(comp)> heap(comp);
-        // for(auto kv : cnts_map)
-        // {
-        //     heap.push(kv.first);
-        // }
-        // vector<int> result;
-        // for(int i=0; i<k; i++){
-        //     result.push_back(heap.top());
-        //     heap.pop();
-        // }
-        // return result;
+        return ans;
     }
 };
