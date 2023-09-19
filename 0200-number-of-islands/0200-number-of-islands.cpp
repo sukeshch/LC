80eb10a276eac918113f5bfd6fffdc89d4c08f42
Sukesh Cheripalli <12154380+sukeshch@users.noreply.github.com>
2023-09-19 17:43:05 -0400

    Time: 34 ms (38.15%), Space: 18.1 MB (32.79%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,57 +1,42 @@
 class Solution {
 public:
-//     int numIslands(vector<vector<char>>& grid) {
-//         // vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size(), false));
-//         vector<vector<int>> dirs{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-//         int res = 0;
-        
-//         for(int i=0; i<grid.size(); i++) {
-//             for(int j=0; j<grid[0].size(); j++) {
-//                 if(grid[i][j] == '1') {
-//                     stack<pair<int,int>> st;
-//                     st.push({i,j});
-//                     grid[i][j] = '0';
-//                     while(!st.empty()) {
-//                         int x = st.top().first, y = st.top().second;
-//                         st.pop();
-//                         for(auto d : dirs) {
-//                             x += d[0], y += d[1];
-//                             if(x >=0 && x < grid.size() && 
-//                                y >= 0 && y < grid[0].size() && 
-//                                grid[x][y] == '1'){ 
-//                                 st.push({x, y});
-//                                 grid[x][y] = '0';
-//                             }
-//                             x -= d[0], y -= d[1];
-//                         }
-//                     }
-//                     res++;
-//                 }
-//             }
-//         }
-//         return res;
-//     }
-    void DFS(vector<vector<char>>& grid, int i, int j) {
-        // cout << i << ", " << j << endl;
-        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size()) return;
-        if(grid[i][j] == '0') return;
-        grid[i][j] = '0';
-        DFS(grid, i+1, j);
-        DFS(grid, i-1, j);
-        DFS(grid, i, j+1);
-        DFS(grid, i, j-1);
-    }
+    vector<pair<int,int>> dirs = {
+        {0, 1}, {0, -1}, {1, 0}, {-1, 0}
+    };
     
-    int numIslands(vector<vector<char>>& grid) {
-        int res = 0;
-        for(int i=0; i<grid.size(); i++) {
-            for(int j=0; j<grid[0].size(); j++) {
-                if(grid[i][j] == '1'){
-                    DFS(grid, i, j);
-                    res++;
+    int R, C;
+    void mark_reachable(vector<vector<char>>& grid, int row, int col) {        
+        stack<pair<int,int>> st; 
+        grid[row][col] = '0';
+        st.push({row, col});
+        
+        while(!st.empty()) {
+            auto top = st.top();
+            st.pop();
+            for(auto dir : dirs) {
+                int row = dir.first + top.first;
+                int col = dir.second + top.second;
+                if( 0 <= row && row < R && 
+                    0 <= col && col < C && grid[row][col] == '1') {
+                    st.push({row, col});
+                    grid[row][col] = '0';
                 }
             }
         }
-        return res;
+    }
+    
+    int numIslands(vector<vector<char>>& grid) {
+        R = grid.size();
+        C = grid[0].size();
+        int num_islands = 0;
+        for(int row = 0; row < grid.size(); row++) {
+            for(int col = 0; col < grid[0].size(); col++) {
+                if(grid[row][col] == '1') {
+                    num_islands++;
+                    mark_reachable(grid, row, col);
+                }
+            }
+        }
+        return num_islands;
     }
 };
