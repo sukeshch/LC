d2329f1f35284875dff9dee52dbf7290c29bdc58
Sukesh Cheripalli <12154380+sukeshch@users.noreply.github.com>
2023-10-02 01:34:28 -0400

    Time: 27 ms (13.82%), Space: 14.7 MB (26.02%) - LeetHub

---
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<int>> adj_;
+    vector<bool> visited_;
+    
+    void bfs(int node) {
+        queue<int> q;
+        
+        q.push(node);
+        visited_[node] = true;
+        
+        while(!q.empty()) {
+            auto top = q.front();
+            q.pop();
+            for(auto c : adj_[top]) {
+                if(!visited_[c]) {
+                    visited_[c] = true;
+                    q.push(c);
+                }
+            }
+        }
+    }
+    
+    int countComponents(int n, vector<vector<int>>& edges) {
+        adj_ = vector<vector<int>>(n, vector<int>());
+        visited_ = vector<bool>(n);
+        
+        for(auto edge : edges) {
+            adj_[edge[0]].push_back(edge[1]);
+            adj_[edge[1]].push_back(edge[0]);
+        }
+        
+        int components = 0;
+        for(int i=0; i<n; i++) {
+            if(!visited_[i]) {
+                bfs(i);
+                components++;
+            }
+        }
+        return components; 
+    }
+};
