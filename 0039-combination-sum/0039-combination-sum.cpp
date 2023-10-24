3c30b2121ad323149c56c69f245a09dbaea343b2
Sukesh Cheripalli <12154380+sukeshch@users.noreply.github.com>
2023-10-23 23:25:36 -0400

    Time: 3 ms (84.74%), Space: 11.2 MB (51.84%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,28 +1,30 @@
 class Solution {
+    int target;
+    vector<int> candidates;
+    vector<vector<int>> result;
 public:
-    void recurse(vector<int> curr, vector<vector<int>>& r, int sum, vector<int>& candidates, int currID)
-    {
-        for(int i=currID; i<candidates.size(); i++)
-        {
-            if(sum < candidates[i])
-                break;
-            else if(sum == candidates[i])
-            {
-                curr.push_back(candidates[i]);
-                r.push_back(curr);
-                return;
-            }
-            auto temp = curr;
-            temp.push_back(candidates[i]);
-            recurse(temp, r, sum-candidates[i], candidates, i);
+    
+    void recurse(int sum, int cur_index, vector<int>& curr) {
+        if(sum == target) {
+            result.push_back(curr);
+            return;
+        }
+        if(sum > target){
+            return;
+        }
+        
+        for(int i=cur_index; i<candidates.size(); i++) {
+            curr.push_back(candidates[i]);
+            recurse(sum+candidates[i], i, curr);
+            curr.pop_back();
         }
     }
     
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        std::sort(candidates.begin(), candidates.end());
-        vector<vector<int>> result;
-        vector<int> temp;
-        recurse(temp, result, target, candidates, 0);
+    vector<vector<int>> combinationSum(vector<int>& c, int t) {
+        target = t;
+        candidates = c;
+        vector<int> curr;
+        recurse(0, 0, curr);
         return result;
     }
 };
