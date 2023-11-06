3069a470993dcb40760e27a11750ca3c5a212f7a
Sukesh Cheripalli <12154380+sukeshch@users.noreply.github.com>
2023-11-05 21:26:55 -0500

    Time: 39 ms (25.18%), Space: 13.7 MB (64.60%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -5,28 +5,28 @@ public:
         int index = 1;
         int cur_count = 1;
         while(index < colors.size()) {
-            if(colors[index-1] == colors[index]) {
+            while(colors[index-1] == colors[index]) {
                 cur_count++;
-            } else {
-                if(cur_count >= 3) {
-                    if (colors[index-1] == 'A') {
-                        acount += (cur_count - 2);
-                    } else {
-                        bcount += (cur_count - 2);
-                    }
-                }
-                cur_count = 1;
+                index++;
             }
+            if(cur_count >= 3) {
+                if (colors[index-1] == 'A') {
+                    acount += (cur_count - 2);
+                } else {
+                    bcount += (cur_count - 2);
+                }
+            }
+            cur_count = 1;
             index++;
         }
-        if(cur_count >= 3) {
-            if (colors[index-1] == 'A') {
-                acount += (cur_count - 2);
-            } else {
-                bcount += (cur_count - 2);
-            }
-        }
-        // cout << "acount : " << acount << " bcount : " << bcount << endl; 
+        // if(cur_count >= 3) {
+        //     if (colors[index-1] == 'A') {
+        //         acount += (cur_count - 2);
+        //     } else {
+        //         bcount += (cur_count - 2);
+        //     }
+        // }
+        cout << "acount : " << acount << " bcount : " << bcount << endl; 
         return (acount > bcount);
     }
 };
