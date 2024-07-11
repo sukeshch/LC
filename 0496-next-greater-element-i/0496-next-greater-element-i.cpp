abb93be79e9adf988c119812f6bc197eb1c4fca6
Sukesh Cheripalli <12154380+sukeshch@users.noreply.github.com>
2024-07-10 21:09:50 -0700

    Time: 4 ms (70.68%), Space: 12.4 MB (6.60%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -23,26 +23,17 @@ public:
             st.push(i);
         }
         
-        std::vector<int> next2_vals;
-        for(auto v : next2) {
-            if(v!=-1){
-                next2_vals.push_back(nums2[v]);
-            }
-            else {
-                next2_vals.push_back(v);
-            }
-        }
-        printvec(next2_vals);
-        
         /* val(1) --> index */
         unordered_map<int, int> mp;
         for(int i=0; i < nums2.size(); i++) {
-            mp[nums2[i]] = i;
+            mp[nums2[i]] = next2[i];
         }
         
         std::vector<int> result(nums1.size(), -1);
         for(int i=0; i < nums1.size(); i++) {
-            result[i] = next2_vals[ mp[nums1[i]] ];
+            if (mp[nums1[i]] != -1) {
+                result[i] = nums2[mp[nums1[i]]];
+            }
         }
         
         return result;
