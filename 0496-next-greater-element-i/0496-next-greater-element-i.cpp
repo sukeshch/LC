fdfee309e0288cb1e30ade19cd4f3df2a93ff93f
Sukesh Cheripalli <12154380+sukeshch@users.noreply.github.com>
2024-07-10 21:03:46 -0700

    Time: 3 ms (82.22%), Space: 12.2 MB (12.44%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,21 +1,50 @@
+#define PRINT_VEC(vec) for(auto v : vec) std::cout << v << " "; 
+
 class Solution {
 public:
+    template <typename T>
+    void printvec(std::vector<T> t) {
+        for(auto v : t) {
+            std::cout << v << ", ";
+        }
+        std::cout << std::endl;
+    }
+    
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> n2_map;
+        // find next greater per element
+        vector<int> next2(nums2.size(), -1);
         stack<int> st;
-        
-        for(int i = 0; i < nums2.size(); i++) {
-            while(!st.empty() && nums2[i] > nums2[st.top()]) {
-                n2_map[nums2[st.top()]] = nums2[i];
+        st.push( 0 ); /* push index */
+        for(auto i = 1; i < nums2.size(); i++) {            
+            while (!st.empty() && nums2[st.top()] < nums2[i] ) { 
+                next2[st.top()] = i;
                 st.pop();
             }
             st.push(i);
         }
-        vector<int> result(nums1.size(), -1);
-        for(int idx = 0; idx < nums1.size(); idx++) {
-            if(n2_map.count(nums1[idx]) != 0)
-                result[idx] = n2_map[nums1[idx]];
+        
+        std::vector<int> next2_vals;
+        for(auto v : next2) {
+            if(v!=-1){
+                next2_vals.push_back(nums2[v]);
+            }
+            else {
+                next2_vals.push_back(v);
+            }
         }
+        printvec(next2_vals);
+        
+        /* val(1) --> index */
+        unordered_map<int, int> mp;
+        for(int i=0; i < nums2.size(); i++) {
+            mp[nums2[i]] = i;
+        }
+        
+        std::vector<int> result(nums1.size(), -1);
+        for(int i=0; i < nums1.size(); i++) {
+            result[i] = next2_vals[ mp[nums1[i]] ];
+        }
+        
         return result;
     }
 };
