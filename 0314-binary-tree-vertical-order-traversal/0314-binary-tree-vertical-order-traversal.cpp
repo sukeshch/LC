b550f18e9a72ec30605c760bdec48e17579e85d6
Sukesh Cheripalli <12154380+sukeshch@users.noreply.github.com>
2024-07-15 21:43:07 -0700

    Time: 3 ms (67.98%), Space: 15.2 MB (20.22%) - LeetHub

---
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    struct node{
+        int row;
+        int col;
+        TreeNode* tree_node;
+    };
+    
+    vector<vector<int>> verticalOrder(TreeNode* root) {
+        if(!root) return {};
+        std::map<int,std::vector<int>> cv_map;
+        queue<node> q;
+        
+        q.push({0, 0, root});
+        
+        while(!q.empty()) {
+            for(int sz = q.size(), i=0; i<sz ; i++) {
+                auto n = q.front();
+                q.pop();
+                cv_map[n.col].push_back(n.tree_node->val);
+                if(n.tree_node->left)
+                    q.push({n.row-1, n.col-1, n.tree_node->left});
+                if(n.tree_node->right)
+                    q.push({n.row+1, n.col+1, n.tree_node->right});
+            }
+        }
+        vector<vector<int>> result;
+        for(auto itr : cv_map) {
+            result.push_back(itr.second);
+        }
+        
+        return result;
+    }
+};
