820dbb15a40cb8248f6e63ee144f5e258040dd5b
Sukesh Cheripalli <12154380+sukeshch@users.noreply.github.com>
2024-07-16 19:38:53 -0700

    Time: 57 ms (41.77%), Space: 25.7 MB (32.53%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    inline bool is_pal(string s, int start, int end) {
+        while(start < end) {
+            if(s[start] != s[end]) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+    
+    bool validPalindrome(string s) {
+        if(s.size() <= 2) return true;
+        
+        int start = 0, end = s.size()-1;
+        int d_count = 0;
+        
+        while(start < end) {
+            if(s[start] == s[end]) {
+                start++;
+                end--;
+            }
+            else {
+                return is_pal(s, start, end-1) || is_pal(s, start+1, end);
+            }
+        }
+        
+        return true;
+    }
+};
