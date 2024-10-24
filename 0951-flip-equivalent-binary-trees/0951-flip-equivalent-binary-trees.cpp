824ad4b5f1d4c999e2d6f9d20585883a55e319f5
Sukesh Cheripalli <12154380+sukeshch@users.noreply.github.com>
2024-10-23 21:09:45 -0700

    Time: 0 ms (100.00%), Space: 14.7 MB (21.11%) - LeetHub

--- 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.cpp
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.cpp
@@ -15,38 +15,48 @@ public:
         if (!root1 && !root2) {
             return true;
         }
-        if (!root1 && root2 || root1 && !root2) {
-            return false;
+        
+        // return (flipEquiv(root1->left, root2->left) &&
+        //         flipEquiv(root1->right, root2->right)) || 
+        //     (flipEquiv(root1->left, root2->right) &&
+        //         flipEquiv(root1->right, root2->left));
+        
+        stack<pair<TreeNode*, TreeNode*>> st;
+        st.push({root1, root2});
+        
+        while(!st.empty()) {
+            auto [n1, n2] = st.top();
+            st.pop();
+            if ((!n1 && n2) || (n1 && !n2)) {
+                return false;
+            }
+
+            if (n1 && n2 && (n1->val != n2->val)) {
+                return false;
+            }
+            
+            auto l1 = n1->left ? n1->left->val : -1;
+            auto l2 = n2->left ? n2->left->val : -1;
+            auto r1 = n1->right ? n1->right->val : -1;
+            auto r2 = n2->right ? n2->right->val : -1;
+            
+            // without swap
+            if(l1 == l2 && r1 == r2) {
+                if (l1 != -1)
+                    st.push({n1->left, n2->left});
+                if(r1 != -1)
+                    st.push({n1->right, n2->right});
+            }
+            else if(l1 == r2 && r1 == l2) {
+                if (l1 != -1)
+                    st.push({n1->left, n2->right});
+                if (r1 != -1)
+                    st.push({n1->right, n2->left});
+            } else {
+                return false;
+            }
         }
         
-        if (root1->val != root2->val) {
-            return false;
-        }
-        
-        return (flipEquiv(root1->left, root2->left) &&
-                flipEquiv(root1->right, root2->right)) || 
-            (flipEquiv(root1->left, root2->right) &&
-                flipEquiv(root1->right, root2->left));
-        
-        /*
-        auto l1 = root1->left->val;
-        auto l2 = root2->left->val;
-        auto r1 = root1->right->val;
-        auto r2 = root2->right->val;
-        
-        // without swap
-        if(l1 == l2 && r1 == r2) {
-            return 
-                flipEquiv(root1->left, root2->left) &&
-                flipEquiv(root1->right, root2->right);
-        }
-        else if(l1 == r2 && r1 == l2) {
-            return 
-                flipEquiv(root1->left, root2->right) &&
-                flipEquiv(root1->right, root2->left);
-        } else {
-            return false;
-        }
-        */
+        return true;
     }
 };
