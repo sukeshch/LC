7d2d1433572b3b10c68cbd80c9be9c23fef45686
Sukesh Cheripalli <12154380+sukeshch@users.noreply.github.com>
2024-12-24 17:59:37 -0800

    Time: 0 ms (100.00%), Space: 95.1 MB (62.03%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,30 +1,42 @@
 class Solution {
 public:
-    inline int getMinAndIncrement(
-        int &ptr1, int &ptr2, 
-        vector<int>& nums1, vector<int>& nums2) {
-        if(ptr1 < nums1.size() && ptr2 < nums2.size()) {
-            if(nums1[ptr1] < nums2[ptr2])
-                return nums1[ptr1++];
-            return nums2[ptr2++];
-        } else if (ptr1 < nums1.size()) {
-            return nums1[ptr1++];
-        } else {
-            return nums2[ptr2++];
-        }   
-    }
-    
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m = nums1.size(), n = nums2.size();
-        bool isOdd = (m + n) % 2;
-        int p1 = 0, p2 = 0;
-        int medianCount = isOdd ? (m+n)/2 : ((m+n)/2-1);
-        while(medianCount) {
-            getMinAndIncrement(p1, p2, nums1, nums2);
-            medianCount--;
+        // median 
+        int N1 = nums1.size(), N2 = nums2.size();
+        if (N1 > N2) {
+            swap(N1, N2);
+            swap(nums1, nums2);
         }
-        if(isOdd)
-            return getMinAndIncrement(p1, p2, nums1, nums2);
-        return ((double)getMinAndIncrement(p1, p2, nums1, nums2) + (double)getMinAndIncrement(p1, p2, nums1, nums2)) / 2;
+        
+        int TN = N1 + N2;
+        
+        int left = 0, right = N1;
+        
+        while (left <= right) {
+            int partitionX = (left + right) / 2;
+            int partitionY = (TN + 1) / 2 - partitionX;
+            
+            int maxL1, minR1, maxL2, minR2; 
+            maxL1 = (partitionX == 0) ? INT_MIN : nums1[partitionX-1];
+            minR1 = (partitionX == N1) ? INT_MAX : nums1[partitionX];
+            
+            maxL2 = (partitionY == 0) ? INT_MIN : nums2[partitionY-1];
+            minR2 = (partitionY == N2) ? INT_MAX : nums2[partitionY];
+            
+            if (maxL1 <= minR2 && maxL2 <= minR1) {
+                if (TN % 2 == 0) {
+                    return (
+                        max(maxL1, maxL2) + min(minR1, minR2)
+                    )/2.0;
+                } else {
+                    return max(maxL1, maxL2);
+                }
+            } else if (maxL1 > minR2) {
+                right = partitionX - 1;
+            } else {
+                left = partitionX + 1;
+            }
+        }
+        return 0.0;
     }
 };
